c9ca3e112cabb7c423c016bc20eca3720d961ce6
shashi kant <skntjee@gmail.com>
2024-12-19 02:11:17 +0530

    adde

--- utils/generateBookingId.js
+++ utils/generateBookingId.js
@@ -2,7 +2,7 @@ const { v4: uuidv4 } = require("uuid");
 
 const generateBookingId = () => {
   // Generate a UUID and simplify it for a more user-friendly booking ID
-  return `ZC${uuidv4().split("-")[0].toUpperCase()}`;
+  return `ZC${uuidv4().split("-")[0].toUpperCase()}`;  
 };
 
 module.exports = generateBookingId;
